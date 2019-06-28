789912bcf491f45c3cf23a9854adf5f4e3ab97aa
DannySuarez <suarezd10@gmail.com>
2019-06-28 12:00:11 -0700

    created HTTP Packet, not tested yet

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const { parseRequest } = require('./lib/app');
 const { createServer } = require('net');
 
+
+const makeHTTPPacket = (data, contentType) => {
+  return `HTTP/1.1 200 OK
+  Date: new Date();
+  Server: Apache
+  Accept-Ranges: bytes
+  Content-Type: ${contentType}
+
+  ${data}`;
+};
+
+
 const server = createServer(sock => {
   console.log('browser connected');
   sock.on('data', data => {
-    parseRequest(data);
-    console.log('parse', parseRequest(data));
+    const req = parseRequest(data);
+    console.log('req', req);
+    if(req.method === 'GET' && req.path === '/') {
+      sock.write('hello');
+      console.log('within if');
+    }
   });
 
 });
