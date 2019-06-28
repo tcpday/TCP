5652f4b2cb1bdacc2e527efeb1aff186865f3d3a
DannySuarez <suarezd10@gmail.com>
2019-06-28 15:26:50 -0700

    cool tangent, browser displays any path you input

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ const server = createServer(sock => {
   console.log('browser connected');
   sock.on('data', data => {
     const req = parseRequest(data);
+    console.log(req);
+    const route = req.path;
+    const realRoute = route.substr(1);
+    console.log('RealRoute', realRoute);
+    console.log('this is route', route);
     console.log('req', req);
     if(req.method === 'GET' && req.path === '/') {
       sock.write(makeHTTPPacket('hi', 'text/text'));
-    } else if(req.method === 'GET' && req.path === '/red') {
-      sock.write(makeHTTPPacket('red', 'text/html'));
+    } else if(req.method === 'GET') {
+      sock.write(makeHTTPPacket(realRoute, 'text/html'));
     }
      
     sock.end();
