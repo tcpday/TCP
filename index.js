1927c2c8f7a1deac893392feab3e2674dfa01f11
DannySuarez <suarezd10@gmail.com>
2019-06-28 17:27:24 -0700

    json part completed

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 const { parseRequest, makeHTTPPacket, makeTextOnlyPacket } = require('./lib/app');
 const { createServer } = require('net');
 
+const bar = {
+  bars: 'scandals',
+  age: 21,
+  drink: 'paloma',
+  weight: 'light'
+};
+
+const jsonBar = JSON.stringify(bar);
+
 const server = createServer(sock => {
-  console.log('browser connected');
   sock.on('data', data => {
     const req = parseRequest(data);
     
     if(req.method === 'GET' && req.path === '/') {
       sock.write(makeTextOnlyPacket('hi', 'text/plain'));
     } else if(req.method === 'POST' && req.path === '/') {
-      sock.write(makeTextOnlyPacket('Sorry!', 'text/plain')); 
+      sock.write(makeTextOnlyPacket('Sorry!', 'text/plain', '400 Bad Request')); 
     } else if(req.method === 'GET' && req.path === '/red') {
       sock.write(makeHTTPPacket('red', 'text/html'));
     } else if(req.method === 'GET' && req.path === '/blue') {
       sock.write(makeHTTPPacket('blue', 'text/html'));
     } else if(req.method === 'GET' && req.path === '/green') {
       sock.write(makeHTTPPacket('green', 'text/html'));
+    } else if(req.method === 'GET' && req.path === '/bar') {
+      sock.write(makeTextOnlyPacket(jsonBar, 'application/json'));
     } else if(req.method === 'GET') {
-      sock.write(makeHTTPPacket('404 NOT FOUND', 'text/html', '404 OK'));
-    }
-     
+      sock.write(makeHTTPPacket('404 NOT FOUND', 'text/html', '404 Not Found'));
+    }  
     sock.end();
   });
 
 });
-
 
 server.listen(7880);
 
