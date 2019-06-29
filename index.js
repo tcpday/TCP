fa5429715c3818aa29914da94f590812bfad00e8
DannySuarez <suarezd10@gmail.com>
2019-06-28 17:06:57 -0700

    test pass error 404

--- index.js
+++ index.js
@@ -5,23 +5,19 @@ const server = createServer(sock => {
   console.log('browser connected');
   sock.on('data', data => {
     const req = parseRequest(data);
-    console.log(req);
-    const route = req.path;
-    const realRoute = route.substr(1);
-    console.log('RealRoute', realRoute);
-    console.log('this is route', route);
-    console.log('req', req);
+    
     if(req.method === 'GET' && req.path === '/') {
-      sock.write(makeTextOnlyPacket('hi', 'text/text'));
+      sock.write(makeTextOnlyPacket('hi', 'text/plain'));
+    } else if(req.method === 'POST' && req.path === '/') {
+      sock.write(makeTextOnlyPacket('Sorry!', 'text/plain')); 
     } else if(req.method === 'GET' && req.path === '/red') {
       sock.write(makeHTTPPacket('red', 'text/html'));
     } else if(req.method === 'GET' && req.path === '/blue') {
       sock.write(makeHTTPPacket('blue', 'text/html'));
     } else if(req.method === 'GET' && req.path === '/green') {
       sock.write(makeHTTPPacket('green', 'text/html'));
-    } else {
-      sock.statusCode = 404;
-      sock.end(makeHTTPPacket('Not Found', 'text/html'));
+    } else if(req.method === 'GET') {
+      sock.write(makeHTTPPacket('404 NOT FOUND', 'text/html', '404 OK'));
     }
      
     sock.end();
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-const { makeHTTPPacket, makeTextOnlyPacket } = require('../lib/app');
+const { makeHTTPPacket, makeTextOnlyPacket, make400Error } = require('../lib/app');
 
 describe('makehttppacket', () => {
   it('will make a header with html', () => {
@@ -20,5 +20,15 @@ describe('routes', () => {
     const html = makeHTTPPacket('red', 'text/html');
     expect(html).toEqual(`${html}`
     );
+  });
+  it('400 text with sorry', () => {
+    global.Date = jest.fn();
+    const text = makeTextOnlyPacket('sorry!', 'text/text');
+    expect(text).toEqual(`${text}`);
+  });
+  it('404 text with error', () => {
+    global.Date = jest.fn();
+    const html = makeHTTPPacket('purple', 'text/html', '404 OK');
+    expect(html).toEqual(`${html}`, 'text/html', '404 OK');
   });
 });
